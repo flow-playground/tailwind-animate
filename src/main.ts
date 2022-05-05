d249277613561f4075cb98be32e63fc586e6ccb4
pensuke <pensuke81@gmail.com>
2022-05-05 11:37:40 +0900

    tailwindcss animation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,40 @@ import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-app.innerHTML = /* HTML */ `
-  <h1 class="text-indigo-500">Hello Vite x tailwindcss</h1>
-`;
+app.innerHTML = /* HTML */ `<div class="container mx-auto">
+  <h1 class="">Animate</h1>
+</div>`;
+
+document.querySelector("#button")?.addEventListener("click", function () {
+  const target = document.querySelector("#target");
+  if (!target) return;
+  if (target.classList.contains("hidden")) {
+    target.classList.add("ease-out");
+    target.classList.remove("hidden");
+    target.animate(
+      [
+        { opacity: 0, transform: "scale(.85)" },
+        { opacity: 1, transform: "scale(1)" },
+      ],
+      {
+        easing: "ease-out",
+        duration: 200,
+      }
+    );
+  } else {
+    const duration = 100;
+    target.animate(
+      [
+        { opacity: 1, transform: "scale(1)" },
+        { opacity: 0, transform: "scale(.85)" },
+      ],
+      {
+        duration: duration,
+        easing: "ease-in",
+      }
+    );
+    setTimeout(() => {
+      target.classList.add("hidden");
+    }, duration);
+  }
+});
