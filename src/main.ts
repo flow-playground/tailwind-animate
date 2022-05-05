68a875fee45d8c094d8227e816a269bd81f2a6dd
Horai <44516902+pensuke81@users.noreply.github.com>
2022-05-05 11:11:18 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = /* HTML */ `
+  <h1 class="text-indigo-500">Hello Vite x tailwindcss</h1>
+`;
